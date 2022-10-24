e0d8bfc40117c127a8754ad37e53ad7766d7485a
HIDRI A <agthidri24@yahoo.com>
2022-10-23 19:53:16 -0500

    this commit will add the first

---
+++ src/main/java/com/example/eregisterapi/ERegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERegisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eregisterapi/ERegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eregisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
